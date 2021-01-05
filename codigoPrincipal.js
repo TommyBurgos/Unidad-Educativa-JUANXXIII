8e7ab334fb358a11fafc8931e5ef79d235741151
Tommy Burgos <trburgos@espol.edu.ec>
2021-01-05 13:24:04 -0500

    Modificacion en la presentación

--- codigoPrincipal.js
+++ codigoPrincipal.js
@@ -1,1 +1,9 @@
+const facebook= document.querySelector(".facebook");
+facebook.setAttribute("title", "facebook");
+
+const instagram = document.querySelector(".instagram");
+instagram.setAttribute("title","Instagram");
+
+const youtube = document.querySelector(".youtube");
+youtube.setAttribute("title","youtube");
 
