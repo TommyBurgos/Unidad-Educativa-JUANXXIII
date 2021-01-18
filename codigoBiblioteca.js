75b3cb891c94ba98e08f0f86220c77dc0d8e3b09
Tommy Burgos <trburgos@espol.edu.ec>
2021-01-18 09:50:06 -0500

    Agregado a la biblioteca

--- codigoBiblioteca.js
+++ codigoBiblioteca.js
@@ -1,12 +1,12 @@
 const contenedor= document.querySelector(".flex-container");
 //,
-const lista= ["El Terror De SextoB","Amigo se escribe con H","Yo nunca digo adios"]
-const listaAutores=["Yolanda reyes","Maria Fernanda Heredia","Maria Fernanda Heredia"];
-const listaGenero=["Cuento","Cuento","Cuento"];
-const ListaEdad1=[8,8,8];
-const ListaEdad2=[10,10,10];
-const ListaImagenes=["ElTerrorDeSextoB.jpg","Amigo_Se_Escribe_con_h.jpg","yo_nunca_digo_adios.jpg"];
-const listaDescarga=["https://drive.google.com/file/d/18C0lIzTPLO1apstdxbkXBkkTJ5qXLc1t/view?usp=sharing","https://drive.google.com/file/d/1QO9DsgJHB3oapFvhOyUlokv5Cc05yHAj/view?usp=sharing","https://drive.google.com/file/d/1RDXGoKjkaNUnVOJyjLpVsaYCFZQ8Dhab/view?usp=sharing"];
+const lista= ["El Terror De SextoB","Amigo se escribe con H","Yo nunca digo adios","La vida es un sueño","De la tierra a la luna"]
+const listaAutores=["Yolanda reyes","Maria Fernanda Heredia","Maria Fernanda Heredia","Pedro calderon de la barca","Julio Verne"];
+const listaGenero=["Cuento","Cuento","Cuento","Literario","Literario"];
+const ListaEdad1=[8,8,8,10,10];
+const ListaEdad2=[10,10,10,11,12];
+const ListaImagenes=["ElTerrorDeSextoB.jpg","Amigo_Se_Escribe_con_h.jpg","yo_nunca_digo_adios.jpg","la-vida-es-un-sueno.jpg","De_la_tierra_a_la_luna.jpg"];
+const listaDescarga=["https://drive.google.com/file/d/18C0lIzTPLO1apstdxbkXBkkTJ5qXLc1t/view?usp=sharing","https://drive.google.com/file/d/1QO9DsgJHB3oapFvhOyUlokv5Cc05yHAj/view?usp=sharing","https://drive.google.com/file/d/1RDXGoKjkaNUnVOJyjLpVsaYCFZQ8Dhab/view?usp=sharing","https://drive.google.com/file/d/13y2na_Uwgwqy5mtx1VEQq5xl70M0G2j8/view?usp=sharing","https://drive.google.com/file/d/14bVXGxOYcbuX9DhXSQLi5yOpkaTIAYev/view?usp=sharing"];
 function crearLibro(nombre, autor,genero, edad1, edad2, imagen,desc){
 	nombre= `<h2>${nombre}</h2>`;
 	autor= `<h3>Autor(a): <p>${autor}</p></h3>`;
