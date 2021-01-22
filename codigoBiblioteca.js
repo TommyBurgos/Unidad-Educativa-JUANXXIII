11c61b276ceab7707efcb28a875cce22f5688a4b
Tommy Burgos <trburgos@espol.edu.ec>
2021-01-22 08:33:00 -0500

    IMAGENEN

--- codigoBiblioteca.js
+++ codigoBiblioteca.js
@@ -5,7 +5,7 @@ const listaAutores=["Yolanda reyes","Maria Fernanda Heredia","Maria Fernanda Her
 const listaGenero=["Cuento","Cuento","Cuento","Literario","Literario"];
 const ListaEdad1=[8,8,8,10,10];
 const ListaEdad2=[10,10,10,11,12];
-const ListaImagenes=["ElTerrorDeSextoB.jpg","Amigo_Se_Escribe_con_h.jpg","yo_nunca_digo_adios.jpg","la-vida-es-un-sueno.jpg","De_la_tierra_a_la_luna.jpg"];
+const ListaImagenes=["el_Terror_De_SextoB.jpg","amigo_se_Escribe_con_h.jpg","yo_nunca_digo_adios.jpg","la-vida-es-un-sueno.jpg","De_la_tierra_a_la_luna.jpg"];
 const listaDescarga=["https://drive.google.com/file/d/18C0lIzTPLO1apstdxbkXBkkTJ5qXLc1t/view?usp=sharing","https://drive.google.com/file/d/1QO9DsgJHB3oapFvhOyUlokv5Cc05yHAj/view?usp=sharing","https://drive.google.com/file/d/1RDXGoKjkaNUnVOJyjLpVsaYCFZQ8Dhab/view?usp=sharing","https://drive.google.com/file/d/13y2na_Uwgwqy5mtx1VEQq5xl70M0G2j8/view?usp=sharing","https://drive.google.com/file/d/14bVXGxOYcbuX9DhXSQLi5yOpkaTIAYev/view?usp=sharing"];
 function crearLibro(nombre, autor,genero, edad1, edad2, imagen,desc){
 	nombre= `<h2>${nombre}</h2>`;
