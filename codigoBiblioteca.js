678343c8570a6f5b33f8d944868de9d33fdb33cb
Tommy Burgos <trburgos@espol.edu.ec>
2021-01-26 10:31:12 -0500

    Mejorando la clase de Youtube

--- codigoBiblioteca.js
+++ codigoBiblioteca.js
@@ -1,15 +1,15 @@
 const contenedor= document.querySelector(".flex-container");
 //,
-const lista= ["El Terror De SextoB","Amigo se escribe con H","Yo nunca digo adios","La vida es un sueño","De la tierra a la luna"]
-const listaAutores=["Yolanda reyes","Maria Fernanda Heredia","Maria Fernanda Heredia","Pedro calderon de la barca","Julio Verne"];
-const listaGenero=["Cuento","Cuento","Cuento","Literario","Literario"];
-const ListaEdad1=[8,8,8,10,10];
-const ListaEdad2=[10,10,10,11,12];
-const ListaImagenes=["el_Terror_De_SextoB.jpg","amigoSeEscribeConH.PNG","yo_nunca_digo_adios.jpg","la-vida-es-un-sueno.jpg","De_la_tierra_a_la_luna.jpg"];
-const listaDescarga=["https://drive.google.com/file/d/18C0lIzTPLO1apstdxbkXBkkTJ5qXLc1t/view?usp=sharing","https://drive.google.com/file/d/1QO9DsgJHB3oapFvhOyUlokv5Cc05yHAj/view?usp=sharing","https://drive.google.com/file/d/1RDXGoKjkaNUnVOJyjLpVsaYCFZQ8Dhab/view?usp=sharing","https://drive.google.com/file/d/13y2na_Uwgwqy5mtx1VEQq5xl70M0G2j8/view?usp=sharing","https://drive.google.com/file/d/14bVXGxOYcbuX9DhXSQLi5yOpkaTIAYev/view?usp=sharing"];
+const lista= ["El Terror De SextoB","Amigo se escribe con H","Yo nunca digo adios","La vida es un sueño","De la tierra a la luna","Huasipungo","Torre de Papel-'R Y M' <h3>investigadores</h3><br>","Pasion por la verdad","El puente de la soledad","El Fantasma de <h3>Canterville</h3><br>"]
+const listaAutores=["Yolanda reyes","Maria Fernanda Heredia","Maria Fernanda Heredia","Pedro calderon de la barca","Julio Verne","Jorge Icaza","Ramon Diaz","Miguel Angel Carlceles","Maria Fernanda Heredia","Oscar Welde"];
+const listaGenero=["Cuento","Cuento","Cuento","Literario","Literario","Literario","Cuento","Literario","Literario","Literario"];
+const ListaEdad1=[8,8,8,10,10,8,8,14,12,13];
+const ListaEdad2=[10,10,10,11,12,10,10,17,15,15];
+const ListaImagenes=["el_Terror_De_SextoB.jpg","amigoSeEscribeConH.PNG","yo_nunca_digo_adios.jpg","la-vida-es-un-sueno.jpg","De_la_tierra_a_la_luna.jpg","huasipungo.jpg","torreDePapel.jpg","pasionPorLaVerdad.jpg","elPuenteDeLaSoledad.jpg","Fantasma-de-Canterville.jpg"];
+const listaDescarga=["https://drive.google.com/file/d/18C0lIzTPLO1apstdxbkXBkkTJ5qXLc1t/view?usp=sharing","https://drive.google.com/file/d/1QO9DsgJHB3oapFvhOyUlokv5Cc05yHAj/view?usp=sharing","https://drive.google.com/file/d/1RDXGoKjkaNUnVOJyjLpVsaYCFZQ8Dhab/view?usp=sharing","https://drive.google.com/file/d/13y2na_Uwgwqy5mtx1VEQq5xl70M0G2j8/view?usp=sharing","https://drive.google.com/file/d/14bVXGxOYcbuX9DhXSQLi5yOpkaTIAYev/view?usp=sharing","https://drive.google.com/file/d/1_pmvbcsNnYP6NBvmPMJUTPiF1CdmYLk_/view?usp=sharing","https://drive.google.com/file/d/1ame7N1uw1IRabpr8cYfLk9v9Hf79vRS9/view?usp=sharing","https://drive.google.com/file/d/1nsMnni0gBb0u3IiLMKgLc3M73ISiueu9/view?usp=sharing","https://drive.google.com/file/d/19tRoqkEdXYyEzldIQr_3lC8XnJoNbWNO/view?usp=sharing","https://drive.google.com/file/d/1wpmeuJdjQRIhDHzdK40M6bZbyhCairML/view?usp=sharing"];
 function crearLibro(nombre, autor,genero, edad1, edad2, imagen,desc){
 	nombre= `<h2>${nombre}</h2>`;
-	autor= `<h3>Autor(a): <p>${autor}</p></h3>`;
+	autor= `<h3>Autor(a):<p>${autor}</p></h3>`;
 	genero= `<h4> ${genero}</h4>`;
 	edad1= edad1;
 	edad2= edad2;
--- codigoYoutube.js
+++ codigoYoutube.js
@@ -4,15 +4,15 @@ const lista= ["Prekinder","kinder","Primero","Segundo","Tercero","Cuarto","Quint
 const listaDescripcion=["Aprenderemos a: <br> *Realizar los primeros trazos <br> *Nociones de colores <br> * Nuestros primero números, y más ","Aprenderemos a: <br> *Realizar los primeros trazos <br> *Nociones de colores <br> * Nuestros primero números, y más ","Aprenderemos a: <br> *Realizar los primeros trazos <br> *Nociones de colores <br> * Nuestros primero números, y más ","Aprenderemos a: <br> *Realizar los primeros trazos <br> *Nociones de colores <br> * Nuestros primero números, y más "];
 const ListaEdad1=[];
 const ListaEdad2=[];
-const ListaImagenes=["portadaPrekinder.png","portadakinder.png","portadaPrimero.png","portadaSegundo.png","portadaTercero.png","portadaCuarto.png","portadaQuinto.png","portadaSexto.png","portadaSeptimo.png"];
-const listaDireccion=["https://www.youtube.com/channel/UCfcJ6_dARpQ3G8eB0I5SnQA","https://www.youtube.com/channel/UCstDXYrhvMrhvNp2uH2YP9Q","https://www.youtube.com/channel/UCQApvOiPUYjxM019zaTPutw","https://www.youtube.com/channel/UCVjYWgqeVQm9x6A95-WPcXw"];
+const ListaImagenes=["portadaPrekinder.png","portadakinder.png","portadaPrimero.png","portadaSegundo.png","portadaTercero.png","portadaCuarto.png","portadaQuinto.png","portadaSexto.png","portadaSeptimo.png","8voBasica.png","9noBasica.png","10moBasica.png","1roBachillerato.png","2doBachillerato.png","3roBachillerato.png"];
+const listaDireccion=["https://www.youtube.com/channel/UCfcJ6_dARpQ3G8eB0I5SnQA","https://www.youtube.com/channel/UCstDXYrhvMrhvNp2uH2YP9Q","https://www.youtube.com/channel/UCQApvOiPUYjxM019zaTPutw","https://www.youtube.com/channel/UCVjYWgqeVQm9x6A95-WPcXw","https://www.youtube.com/channel/UCf2JOYzLDcrB5-MD4GiXFFw","https://www.youtube.com/channel/UCIPJUFzOOD9JaIS7mQlT8Vg","https://www.youtube.com/channel/UCeTLR2JnU-IFoga7MeAiB_w/videos","https://www.youtube.com/channel/UCpek3BFWNXt_EWVJTJavDQg/videos","https://www.youtube.com/channel/UCfshcfNL1HW-s3vHRa_Wf6w","https://www.youtube.com/channel/UCq8xZVp9ApXOvzQWnOO-BfA","https://www.youtube.com/channel/UCQoCsCFZsF6sZ-3T7Z7jhog","https://www.youtube.com/channel/UC3h8pFakLqRfL-51_l_0gQg","https://www.youtube.com/channel/UCr0f8LuLnkXKPiyXkXD8MfQ","https://www.youtube.com/channel/UCo0ND6jHHL072_TNjEUTtHw","https://www.youtube.com/channel/UCzYaLmP4ukpC0XCoquSJgFg"];
 function crearLibro(nombre ,descripcion, edad1, edad2, imagen,direc){
 	nombre= `<h2 class="nombre">${nombre}</h2>`;
 	descripcion= `<h4> ${descripcion}</h4>`;
 	edad1= edad1;
 	edad2= edad2;
 	imagen= `<img class='canal_img' src="${imagen}" >`;
-	direc= `<a href="${direc}"> <button class="ir"> Ir al canal --> </button> </a>`;
+	direc= `<a href="${direc}"> <button class="ir"> IR AL CANAL <i class="fas fa-arrow-right"></i> </button> </a>`;
 
 	return [imagen,nombre,descripcion,direc];
 }
