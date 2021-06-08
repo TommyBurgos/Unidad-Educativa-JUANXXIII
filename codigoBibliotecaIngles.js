82597e8310e21c8b62dcf8f5a0d05a660d62bdc5
TommyBurgos <trburgos@espol.edu.ec>
2021-06-08 10:12:26 -0500

    Se crea la división en el plan lector

---
+++ codigoBibliotecaIngles.js
@@ -0,0 +1,179 @@
+const contenedor= document.querySelector(".flex-container");
+const basInf= document.querySelector(".flex-containerBI");
+const contenedorBE= document.querySelector(".flex-containerBE");
+const contenedorBS= document.querySelector(".flex-containerBS");
+const contenedorBGU= document.querySelector(".flex-containerBGU");
+const contador=document.querySelector(".contador");
+//,
+const lista= ["FactFiles-New York","The Canterville-Ghost","the_happy_prince_and_other_tales"];
+const listaAutores=["John Scoot","Oscar Wilde","Oscar Wilde"];
+const listaGenero=["Informativo","Litario","Litario"];
+const ListaEdad1=[9,12,12];
+const ListaEdad2=[11,15,14];
+const ListaImagenes=["Imagenes/Biblioteca/Ingles/newYork.jpg","Imagenes/Biblioteca/Ingles/theCantervilleGhost.jpg","Imagenes/Biblioteca/Ingles/the_happy_prince_and_other_tales.jpg"];
+const listaDescarga=["https://drive.google.com/file/d/1leRshgFQuGMRXEe0rm9OilRzhZtNabml/view?usp=sharing","https://drive.google.com/file/d/1jv13DM_FUEIgsEIsIDT5DretqMaKqtY1/view?usp=sharing","https://drive.google.com/file/d/1zLtGeTBG-DwQRWm0UNKr4iZWjs7iVbJQ/view?usp=sharing"];
+
+
+function crearLibro(nombre, autor,genero, edad1, edad2, imagen,desc){
+	nombre= `<h2 class="nombre">${nombre}</h2>`;
+	autor= `<h3>Autor(a):<p>${autor}</p></h3>`;
+	genero= `<h4> ${genero}</h4>`;
+	edad1= edad1;
+	edad2= edad2;
+	imagen= `<img class='libros_img' src="${imagen}" >`;
+	desc= `<a> href="${desc}" </a>`;
+
+	return [imagen,nombre,autor,genero,edad1,edad2];
+}
+class libro{
+	constructor(nombre, autor,genero, edad1, edad2, imagen,desc){
+		this.nombre=nombre;
+		this.autor=autor;
+		this.genero=genero;
+		this.edad1=edad1;
+		this.edad2=edad2;
+		this.imagen=imagen;
+		this.desc=desc;
+	}
+}
+const changeHidden=(descarga)=>{
+	document.querySelector(".descargarlib").href= `${descarga}`;
+};
+
+let documentFragmentG = document.createDocumentFragment();
+let documentFragmentBI = document.createDocumentFragment();
+let documentFragmentBE= document.createDocumentFragment();
+let documentFragmentBS = document.createDocumentFragment();
+let documentFragmentBACH = document.createDocumentFragment();
+
+let ctBI=0;
+let ctBM=0;
+let ctBS=0;
+let ctBGU=0;
+let ctGeneral=0;
+
+for (var i = 0; i <lista.length; i++) {
+	let nombre= lista[i];
+	let autor= listaAutores[i];
+	let genero= listaGenero[i];
+	let edad1= ListaEdad1[i];
+	let edad2= ListaEdad2[i];
+	let imagen= ListaImagenes[i];
+	let descarga= listaDescarga[i];
+	let libro= crearLibro(nombre,autor,genero,edad1,edad2,imagen,descarga);
+	if (edad1>=6 && edad1<=8) {
+		let divBI= document.createElement("DIVBI");
+		let vacio= document.createElement("DIVBI");
+		divBI.addEventListener("click",()=>(changeHidden(descarga)))
+		divBI.tabIndex = i;
+		divBI.classList.add(`item-${i}`,'flex-item');
+		divBI.innerHTML= libro[0]+ libro[1]+libro[2];
+		documentFragmentBI.appendChild(divBI);
+		ctBI ++
+
+		
+	}if((edad2>=9 && edad2<=11) || (edad1>=9 && edad1<=11)){
+		let divBE= document.createElement("DIVBE");			
+		divBE.addEventListener("click",()=>(changeHidden(descarga)))
+		divBE.tabIndex = i;
+		divBE.classList.add(`item-${i}`,'flex-item');
+		divBE.innerHTML= libro[0]+ libro[1]+libro[2];
+		documentFragmentBE.appendChild(divBE);
+		ctBM ++
+
+	}
+	if((edad1>=12 && edad1<=14) || (edad2>=12 && edad2<=14)){
+		let divBS= document.createElement("DIVBS");
+		divBS.addEventListener("click",()=>(changeHidden(descarga)))
+		divBS.tabIndex = i;
+		divBS.classList.add(`item-${i}`,'flex-item');
+		divBS.innerHTML= libro[0]+ libro[1]+libro[2];
+		documentFragmentBS.appendChild(divBS);
+		ctBS ++
+
+	}
+	if (edad2>14) {
+		let divBach= document.createElement("DIVBach");
+		divBach.addEventListener("click",()=>(changeHidden(descarga)))
+		divBach.tabIndex = i;
+		divBach.classList.add(`item-${i}`,'flex-item');
+		divBach.innerHTML= libro[0]+ libro[1]+libro[2];
+		documentFragmentBACH.appendChild(divBach);
+		ctBGU ++
+	}
+
+	let div= document.createElement("DIV");
+	div.addEventListener("click",()=>(changeHidden(descarga)))
+	div.tabIndex = i;
+	div.classList.add(`item-${i}`,'flex-item');
+	div.innerHTML= libro[0]+ libro[1]+libro[2];
+	documentFragmentG.appendChild(div);
+	ctGeneral++;
+	
+};
+basInf.appendChild(documentFragmentBI);
+contenedor.appendChild(documentFragmentG);
+contenedorBE.appendChild(documentFragmentBE);
+contenedorBS.appendChild(documentFragmentBS);
+contenedorBGU.appendChild(documentFragmentBACH);
+contador.innerHTML='<p><b>Total de libros: </b></p>' + ctGeneral;
+
+let botonDescarga= document.querySelector('descargarlib');
+
+
+const basicaInferior= document.querySelector(".flex-itemBI");
+const botonBI= document.querySelector(".basicaInferior");
+const botonBE= document.querySelector(".basicaElemental");
+const botonBS= document.querySelector(".basicaSuperior");
+const botonBGU= document.querySelector(".bachillerato");
+const botonSF=document.querySelector(".vaciar");
+
+botonBI.addEventListener("click",(e)=>{
+	contenedor.style='display:none' ;
+	basInf.style='display:flex';
+	contenedorBS.style='display:none';
+	contenedorBGU.style='display:none';
+	contenedorBE.style='display:none';
+	contador.style='display:flex';
+	contador.innerHTML='<p><b> Cantidad de resultados obtenidos: </b> </p>' + ctBI;
+	
+})
+botonBE.addEventListener("click",(e)=>{
+	contenedor.style='display:none' ;
+	basInf.style='display:none';
+	contenedorBS.style='display:none';
+	contenedorBGU.style='display:none';
+	contenedorBE.style='display:flex';
+	contador.style='display:flex';
+	contador.innerHTML='<p><b> Cantidad de resultados obtenidos: </b> </p>' + ctBM;
+})
+botonBS.addEventListener("click",(e)=>{
+	contenedor.style='display:none' ;
+	basInf.style='display:none';
+	contenedorBS.style='display:flex';
+	contenedorBGU.style='display:none';
+	contenedorBE.style='display:none';
+	contador.style='display:flex';
+	contador.innerHTML='<p><b> Cantidad de resultados obtenidos: </b> </p>' + ctBS;
+})
+botonBGU.addEventListener("click",(e)=>{
+	contenedor.style='display:none' ;
+	basInf.style='display:none';
+	contenedorBS.style='display:none';
+	contenedorBGU.style='display:flex';
+	contenedorBE.style='display:none';
+	contador.style='display:flex';
+	contador.innerHTML='<p><b> Cantidad de resultados obtenidos: </b> </p>' + ctBGU;
+})
+botonSF.addEventListener("click",(e)=>{
+	contenedor.style='display:flex' ;
+	basInf.style='display:none';
+	contenedorBS.style='display:none';
+	contenedorBGU.style='display:none';
+	contenedorBE.style='display:none';
+	contador.style='display:none';
+	contador.style='display:flex';
+	contador.innerHTML='<p><b>Total de libros: </b></p>' + ctGeneral;
+
+})
+
