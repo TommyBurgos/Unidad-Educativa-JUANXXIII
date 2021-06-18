4c5b67aa38a2e7b97758a53b47ae735056186810
TommyBurgos <trburgos@espol.edu.ec>
2021-06-18 12:03:36 -0500

    Se crea nueva opción de fabulas

--- codigoBibliotecaInicial.js
+++ codigoBibliotecaInicial.js
@@ -5,27 +5,19 @@ const contenedorBS= document.querySelector(".flex-containerBS");
 const contenedorBGU= document.querySelector(".flex-containerBGU");
 const contador=document.querySelector(".contador");
 //,
-const lista= ["Ajedrez logico","Ajedrez HiperModerno 1","Ajedrez HiperModerno 2","Los secretos de la estrategia moderna en Ajedrez","Fundamentos de Ajedrez","Los 100 finales que hay que saber","222 celadas en las aperturas","Las mil y una lecciones de ajedrez","Ajedrez de los grandes maestros jugada a jugada","La pasión del Ajedrez",
-"101miniaturasBrillantes.png","El ajedrez de torneo","Princesa Del Ajedrez","Fundamentos estrategicos de Ajedrez","24 lecciones de Ajedrez","Entender las Aperturas.jpg","Mi sistema Ninsowitch","Descubrir las aperturas"];
-const listaAutores=["Irving Chernev ", "R. Aguilera y R.J. Perez","R. Aguilera y Francisco Jose Perez","John Wattson","Enrique P. Falcon","Jesus de la villa","Karsten Muller","Mi angel Martin","John Nunn","Gary Casparov",
-"John Nunn","D.Bronstein","Judit Polgar","Y.B. Estrin","Garri Kasparov","Sam Collins","AAron Ninsowitch","john emms"];
-const listaGenero=["Informativa","Informativa","Informativa","Informativa","Informativa","Informativa","Informativa","Informativa","Informativa","Informativa",
-"Informativa","Informativa","Literario","Informativa","Informativa","Informativa","Informativa","Informativa"];
-const ListaEdad1=[12,12,12,12,12,12,12,12,12,12,
-				  12,12,12,12,12,12,12,12];
-const ListaEdad2=[17,17,17,17,17,17,17,17,17,17,
-				  17,17,17,17,17,17,17,17];
-const ListaImagenes=["Imagenes/Biblioteca/Ajedrez/AjedrezLogico.jpg","Imagenes/Biblioteca/Ajedrez/AjedrezModerno.JPG","Imagenes/Biblioteca/Ajedrez/AjedrezHipermoderno2.jpg","Imagenes/Biblioteca/Ajedrez/secretosAjedrez.jpg","Imagenes/Biblioteca/Ajedrez/fundamentosAjedrez.jpg","Imagenes/Biblioteca/Ajedrez/los100Finales.jpg","Imagenes/Biblioteca/Ajedrez/222.jpg","Imagenes/Biblioteca/Ajedrez/lasMilYunaAventura.jpg","Imagenes/Biblioteca/Ajedrez/ajedrezdelosgrandesmaestros.jpg",
-"Imagenes/Biblioteca/Ajedrez/laPasionDelAjedrez.jpg","Imagenes/Biblioteca/Ajedrez/101miniaturasBrillantes.png","Imagenes/Biblioteca/Ajedrez/elAjedrezdeTorneo.png","Imagenes/Biblioteca/Ajedrez/princesaDelAjedrez.jpg","Imagenes/Biblioteca/Ajedrez/estrategias.png","Imagenes/Biblioteca/Ajedrez/24Lecciones.jpg","Imagenes/Biblioteca/Ajedrez/entenderAperturas.jpg","Imagenes/Biblioteca/Ajedrez/miSistema.jpg","Imagenes/Biblioteca/Ajedrez/descubrirLasAperturas.jpg"];
-const listaDescarga=["https://drive.google.com/file/d/1t3UU5zTuLLfV0M2CN1X2pAytOp2X_JlM/view?usp=sharing","https://drive.google.com/file/d/1-5QSmVKxi3m6IkITXcWcBDwERPPq118U/view?usp=sharing","https://drive.google.com/file/d/1BSMsAr8Gqi70beNQz-l0ZVInsP8tXXss/view?usp=sharing","https://drive.google.com/file/d/1jU1pNbw01JEWeLqoyAaR0l8S0Yw5b12U/view?usp=sharing","https://drive.google.com/file/d/1vNnv9PPR88r5tPZ5KsQUbmVNUwpbi-OU/view?usp=sharing",
-"https://drive.google.com/file/d/1DqunFiNzyreUijgqEGqtDr7xYLLhph3C/view?usp=sharing","https://drive.google.com/file/d/1DLhTV-pFG_wqoyvJPk9IWo5WDqAcjXNB/view?usp=sharing","https://drive.google.com/file/d/1ikCmIAF9MBuj3YeLbhyzuC_V809oi2XP/view?usp=sharing","https://drive.google.com/file/d/14s14728cbMv4GtEQWqKnO3kqR8H6RICF/view?usp=sharing","https://drive.google.com/file/d/1RhzacCxSAUJ2SmNthmp0IzdAfgoWonvB/view?usp=sharing",
-"https://drive.google.com/file/d/17sLbQJw1t8uI3uRFnLhqM8FdCjmkIwF0/view?usp=sharing","https://drive.google.com/file/d/1XxDxxaNBQvIf3bm2VSNTxnKwK78f8wwP/view?usp=sharing","https://drive.google.com/file/d/1CYHMeiU6t9M72noX-lfccmBt1dGwManl/view?usp=sharing","https://drive.google.com/file/d/1x8Hk2mWawR72GLGgAyldGCEQKp2Ql102/view?usp=sharing","https://drive.google.com/file/d/15SuXEKfrEO7UOXPEanubY5mvGilu1rv5/view?usp=sharing",
-"https://drive.google.com/file/d/16U4TWE27SobZc2I-pE3uu-08BtGT3_65/view?usp=sharing","https://drive.google.com/file/d/1CtIZCw2lMB5idvqjaCmHKS4pHtiqMGLa/view?usp=sharing","https://drive.google.com/file/d/1enor8Jac_fqpNG5EHSkOggL4BDVQVPht/view?usp=sharing"];
-
+const lista= ["Serenata a una Perrita","El vendedor de Alfombras","<br><br>Una Aguja en un pajar","El matrimonio Oson","El tigrito que se mordia las uñas","El buen tiempo y la lluvia","<br>Las patas de un elefante","El gato, el Zorro y el gallo","<br>El gatito glotón"];
+const listaAutores=[1,2,3,4,5,6,7,8,9];
+const listaGenero=["Fabulas","Fabulas","Fabulas","Fabulas","Fabulas","Fabulas","Fabulas","Fabulas","Fabulas"];
+const ListaEdad1=[4,4,4,4,4,4,4,4,4];
+const ListaEdad2=[8,8,8,8,8,8,8,8,8];
+const ListaImagenes=["Imagenes/Biblioteca/Inicial/serenata.jpg","Imagenes/Biblioteca/Inicial/cerdoVendedor.jpg","Imagenes/Biblioteca/Inicial/unaAgujaEnUnPajar.jpg","Imagenes/Biblioteca/Inicial/matrimonioOson.jpg","Imagenes/Biblioteca/Inicial/elTigrito.jpg",
+"Imagenes/Biblioteca/Inicial/buenTiempoyLLuvia.jpg","Imagenes/Biblioteca/Inicial/patasElefante.jpg","Imagenes/Biblioteca/Inicial/elGato,gallina.jpg","Imagenes/Biblioteca/Inicial/gatitoGloton.jpg"];
+const listaDescarga=["https://drive.google.com/file/d/1EBhz_1wdQfYrpSPx1dQQNE-SrWyyf61A/view?usp=sharing","https://drive.google.com/file/d/13sUyEBlRvOBT2ZoDFUvF-dIw-SoKhmu3/view?usp=sharing","Imagenes/Biblioteca/Inicial/","https://drive.google.com/file/d/1JGDSEL9JP9xf2S4WP9OJKJa6C6uleyqy/view?usp=sharing","https://drive.google.com/file/d/1KRBM0MgbgQJxys0RVSLrNd0N2ffongCj/view?usp=sharing",
+"https://drive.google.com/file/d/1mycnx4h94vm30m0lXEW3sICi4zFDNk8i/view?usp=sharing","https://drive.google.com/file/d/1AsiRZ7frZrSrBCU092N8y2PkLjyXrrVJ/view?usp=sharing","https://drive.google.com/file/d/1vEWsLf0u4F0o-FV1Pvcqw3WcQtg5V6Q2/view?usp=sharing","https://drive.google.com/file/d/1EHE0TxPoAYxbwz_k3dRE-cyBLi9P3cfz/view?usp=sharing"];
 
 function crearLibro(nombre, autor,genero, edad1, edad2, imagen,desc){
 	nombre= `<h2 class="nombre">${nombre}</h2>`;
-	autor= `<h3>Autor(a):<p>${autor}</p></h3>`;
+	autor= `<h3>Fabula 365- Día:<p>${autor}</p></h3>`;
 	genero= `<h4> ${genero}</h4>`;
 	edad1= edad1;
 	edad2= edad2;
@@ -70,7 +62,7 @@ for (var i = 0; i <lista.length; i++) {
 	let imagen= ListaImagenes[i];
 	let descarga= listaDescarga[i];
 	let libro= crearLibro(nombre,autor,genero,edad1,edad2,imagen,descarga);
-	if (edad1>=6 && edad1<=8) {
+	if (edad1>=4 && edad1<=6) {
 		let divBI= document.createElement("DIVBI");
 		let vacio= document.createElement("DIVBI");
 		divBI.addEventListener("click",()=>(changeHidden(descarga)))
@@ -81,7 +73,7 @@ for (var i = 0; i <lista.length; i++) {
 		ctBI ++
 
 		
-	}if((edad2>=9 && edad2<=11) || (edad1>=9 && edad1<=11)){
+	}if((edad2>=6 && edad2<=8) || (edad1>=6 && edad1<=8)){
 		let divBE= document.createElement("DIVBE");			
 		divBE.addEventListener("click",()=>(changeHidden(descarga)))
 		divBE.tabIndex = i;
@@ -91,26 +83,6 @@ for (var i = 0; i <lista.length; i++) {
 		ctBM ++
 
 	}
-	if((edad1>=12 && edad1<=14) || (edad2>=12 && edad2<=14)){
-		let divBS= document.createElement("DIVBS");
-		divBS.addEventListener("click",()=>(changeHidden(descarga)))
-		divBS.tabIndex = i;
-		divBS.classList.add(`item-${i}`,'flex-item');
-		divBS.innerHTML= libro[0]+ libro[1]+libro[2];
-		documentFragmentBS.appendChild(divBS);
-		ctBS ++
-
-	}
-	if (edad2>14) {
-		let divBach= document.createElement("DIVBach");
-		divBach.addEventListener("click",()=>(changeHidden(descarga)))
-		divBach.tabIndex = i;
-		divBach.classList.add(`item-${i}`,'flex-item');
-		divBach.innerHTML= libro[0]+ libro[1]+libro[2];
-		documentFragmentBACH.appendChild(divBach);
-		ctBGU ++
-	}
-
 	let div= document.createElement("DIV");
 	div.addEventListener("click",()=>(changeHidden(descarga)))
 	div.tabIndex = i;
@@ -185,4 +157,7 @@ botonSF.addEventListener("click",(e)=>{
 	contador.innerHTML='<p><b>Total de libros: </b></p>' + ctGeneral;
 
 })
+/*
 
+
+*/
